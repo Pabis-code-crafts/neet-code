fb83763aa3b902e75c31d9b5c45d384924a47749
Pabis-code-crafts <pavithrangurusamy21@gmail.com>
2026-01-18 15:17:10 +0000

    Climbing Stairs

---
+++ src/main/java/org/example/climbingStairs/ClimbingStairs.java
@@ -0,0 +1,16 @@
+package org.example.climbingStairs;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        int[] steps = {1,2};
+        int target = 5;
+        int[] dp = new int[target+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+         for (int s = 3; s <= target; s++) {
+            dp[s] = dp[s-1] + dp[s-2];
+        }
+        System.out.println(dp[target]);
+    }
+}
---
+++ src/main/java/org/example/fibonaci/Fibonachi.java
@@ -0,0 +1,18 @@
+package org.example.fibonaci;
+
+import java.util.Arrays;
+
+public class Fibonachi {
+    public static void main(String[] args) {
+        int val1 = 0;
+        int val2 = 1;
+        int target = 10;
+        int[] list = new int[target];
+        list[0] = val1;
+        list[1] = val2;
+        for (int i = 2; i < target; i++) {
+            list[i] = list[i-1]+list[i-2];
+        }
+        System.out.println(Arrays.toString(list));
+    }
+}
