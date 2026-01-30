537a1e0e479a5616795b0ec96a796c94c0eb2b75
Pabis-code-crafts <pavithrangurusamy21@gmail.com>
2026-01-30 19:11:54 +0000

    Back Tracking

--- src/main/java/org/example/backtracking/letterCase/LetterCase.java
+++ src/main/java/org/example/backtracking/letterCase/LetterCase.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LetterCase {
+
+
+
     public static void main(String[] args) {
+
         String s = "a1b";
         List<String> res = new ArrayList<>();
         backtrack(s.toCharArray(), 0, res);
-        System.out.println(res);
+        System.out.println("FINAL RESULT → " + res);
+
     }
 
     static void backtrack(char[] arr, int index, List<String> res) {
@@ -16,22 +21,20 @@ public class LetterCase {
             res.add(new String(arr));
             return;
         }
+        if(Character.isLetter(arr[index])){
 
-        // If letter → two choices
-        if (Character.isLetter(arr[index])) {
-
-            // lowercase
             arr[index] = Character.toLowerCase(arr[index]);
-            backtrack(arr, index + 1, res);
+            backtrack( arr, index+1, res);
 
-            // uppercase
             arr[index] = Character.toUpperCase(arr[index]);
-            backtrack(arr, index + 1, res);
-
-        } else {
-            // digit → only one path
-            backtrack(arr, index + 1, res);
+            backtrack( arr, index+1, res);
+        }else{
+            backtrack( arr, index+1, res);
         }
+
+
+
+
+
     }
 }
-
