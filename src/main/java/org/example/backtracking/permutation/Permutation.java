195076e4b41702253c3fa09cb5befe0d9977bd27
Pabis-code-crafts <pavithrangurusamy21@gmail.com>
2026-02-02 17:54:55 +0000

    permutation

---
+++ src/main/java/org/example/backtracking/permutation/Permutation.java
@@ -0,0 +1,41 @@
+package org.example.backtracking.permutation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.swap;
+
+public class Permutation {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        List<List<Integer>> res = new ArrayList<>();
+
+        btPermutation(nums, 0, res);
+
+        System.out.println(res);
+    }
+
+    private static void btPermutation(int[] nums, int index, List<List<Integer>> res) {
+
+        if (nums.length == index) {
+            ArrayList<Integer> curr = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                curr.add(nums[i]);
+            }
+            res.add(curr);
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            swapNums(i,index, nums);
+            btPermutation(nums,index+1,res);
+            swapNums(i,index, nums);
+        }
+
+    }
+
+    private static void swapNums(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
