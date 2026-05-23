0f78fc9307bfbf3a682e15a14aae8fa62f46f170
Pabis-code-crafts <pavithrangurusamy21@gmail.com>
2026-05-23 11:16:38 +0100

    container

--- src/main/java/org/example/prac/prac.java
+++ src/main/java/org/example/prac/prac.java
@@ -4,37 +4,23 @@ import java.util.*;
 
 public class prac {
     public static void main(String[] args) {
+        int container[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int l = 0;
+        int r = container.length-1;
+        int maxhold = 0;
 
-        char[][] grid = {
-                {'1','1','1','1','0'},
-                {'1','1','0','1','0'},
-                {'1','1','0','0','0'},
-                {'0','0','0','1','0'}
-        };
 
-        int[][] dir = {{-1,0},{0,-1},{1,0},{0,1}};
-        int islands = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j]=='1'){
-                    islands++;
-                    islandchecker(grid, i, j);
-                }
-            }
+        while(l<r){
+            int surface = r -l;
+            int holdheight = Math.min(container[r],container[l]);
+            maxhold = Math.max(surface*holdheight,maxhold);
+            if(container[r]>container[l]){
+                l++;
+            }else r--;
+
+
         }
-        System.out.println("islands : " + islands);
-    }
-
-    private static void islandchecker(char[][] grid, int row, int col) {
-        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0'){
-            return;
-        }
-        grid[row][col] = '0';
-        islandchecker(grid,row-1,col);
-        islandchecker(grid,row,col-1);
-        islandchecker(grid,row,col+1);
-        islandchecker(grid,row+1,col);
-
+        System.out.println(maxhold);
     }
 
 
